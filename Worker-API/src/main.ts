c4d533649eaa8edeb370f83cb7bd80e4f3d010dd
chabuuuu <haphuthinh332004@gmail.com>
2024-01-15 11:38:55 +0700

    config cors

--- Worker-API/src/main.ts
+++ Worker-API/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './routes';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ReceiveMessageService } from './services/pubsub/receive.service';
+import methods from 'cache-manager-redis-store';
+import helmet from "helmet";
 const morgan = require('morgan');
 var cors = require('cors');
 var bodyParser = require('body-parser');
@@ -10,10 +12,18 @@ const config = require('config');
 //const requestLimit = jsonRequestLimitconfig.limit;
 const requestLimit = config.get('request-limit.limit');
 
+var corsOptions = {
+  origin: config.get('cors.origin'),
+  methods: config.get('cors.methods'),
+}
+console.log(corsOptions);
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(morgan('dev'));
-  app.use(cors());
+  app.use(helmet());
+  //app.use(cors(corsOptions));
+  app.enableCors(corsOptions);
   app.use(bodyParser.urlencoded({ limit: requestLimit, extended: true }));
   const receiveMessage = new ReceiveMessageService();
   receiveMessage.receiveNoti();
